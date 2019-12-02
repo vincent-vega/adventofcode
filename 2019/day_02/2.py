1225374a059f63a73e858d23c7f846ed9b939e87
Davide Barelli <dbarelli@wersec.com>
2019-12-02 19:00:57 +0000

    day 2

---
+++ 2019/day_02/2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from itertools import product
+
+def _run_op(values, instruction_pointer):
+    values[values[instruction_pointer + 3]] = {
+        1: values[values[instruction_pointer + 1]] + values[values[instruction_pointer + 2]],
+        2: values[values[instruction_pointer + 1]]*values[values[instruction_pointer + 2]]
+    }.get(values[instruction_pointer])
+
+def _init(values, noun, verb):
+    values[1] = noun
+    values[2] = verb
+
+def _run_prog(values, noun, verb):
+    _init(values, noun, verb)
+    for i in range(0, len(values), 4):
+        if values[i] == 99:
+            break
+        _run_op(values, i)
+    return values[0]
+
+def part1(values):
+    return _run_prog(values, 12, 2)
+
+def part2(values):
+    for noun, verb in product(range(100), range(100)):
+        if _run_prog(list(values), noun, verb) == 19690720:
+            return 100*noun + verb
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        values = list(map(int, f.read().split(',')))
+    print(part1(list(values))) # 6087827
+    print(part2(values)) # 5379
+
---
+++ 2019/day_02/Day02.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Day02
+{
+    private static void runOp(Integer[] o, int opPos)
+    {
+        switch (o[opPos]) {
+        case 1:
+            o[o[opPos + 3]] = o[o[opPos + 1]] + o[o[opPos + 2]];
+            break;
+        case 2:
+            o[o[opPos + 3]] = o[o[opPos + 1]]*o[o[opPos + 2]];
+            break;
+        default:
+            throw new IllegalArgumentException(String.format("Unknown operation code", o[opPos]));
+        }
+    }
+
+    private static void init(Integer[] o, int noun, int verb)
+    {
+        o[1] = noun;
+        o[2] = verb;
+    }
+
+    private static int runProgram(Integer[] operations, int noun, int verb)
+    {
+        init(operations, noun, verb);
+        for (int i = 0; i < operations.length && operations[i] != 99; i += 4)
+            runOp(operations, i);
+        return operations[0];
+    }
+
+    public static String part1(List<Integer> values)
+    {
+        return String.valueOf(runProgram(values.toArray(new Integer[0]), 12, 2));
+    }
+
+    public static String part2(List<Integer> values)
+    {
+        for (int noun = 0; noun < 100; noun++)
+            for (int verb = 0; verb < 100; verb++)
+                if (runProgram(values.toArray(new Integer[0]), noun, verb) == 19690720)
+                    return String.valueOf(100*noun + verb);
+        throw new IllegalArgumentException("Final value not found");
+    }
+
+    public static void main(String[] args)
+    {
+        List<Integer> values = new ArrayList<>();
+        try (Scanner sc = new Scanner(new File("input.txt"))){
+            while (sc.hasNext())
+                Stream.of(sc.next().split(",")).map(n -> Integer.valueOf(n)).forEach(n -> values.add(n));
+            System.out.println(part1(values)); // 6087827
+            System.out.println(part2(values)); // 5379
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
