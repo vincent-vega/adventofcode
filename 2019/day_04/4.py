045887a2053c684e959f25cbb1af66461b09dc5e
Davide Barelli <dbarelli@wersec.com>
2019-12-04 21:42:34 +0000

    day 4

---
+++ 2019/day_04/4.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+def part1(min_N, max_N):
+    def _rules(n):
+        n = str(n)
+        if len(n) != 6:
+            return False
+        found_double = False
+        for i in range(len(n) - 1):
+            if n[i] == n[i + 1]:
+                found_double = True
+            if int(n[i]) > int(n[i + 1]):
+                return False
+        else:
+            return found_double
+
+    return len(list(filter(_rules, range(min_N, max_N + 1))))
+
+def part2(min_N, max_N):
+    def _rules(n):
+        n = str(n)
+        if len(n) != 6:
+            return False
+        prev = None
+        found_double = False
+        for i in range(len(n) - 1):
+            if n[i] == n[i + 1] and (i == len(n) - 2 or int(n[i + 1]) != int(n[i + 2])) and n[i] != prev:
+                found_double = True
+            if int(n[i]) > int(n[i + 1]):
+                return False
+            prev = n[i]
+        else:
+            return found_double
+
+    return len(list(filter(_rules, range(min_N, max_N + 1))))
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        min_N, max_N = list(map(int, re.findall(r'\d+', f.read())))
+    print(part1(min_N, max_N)) # 895
+    print(part2(min_N, max_N)) # 591
+
