b13c37d7c05a183fa20016f4dcb5a51a9242df0c
Davide Barelli <bar.davide@gmail.com>
2020-06-28 19:26:15 +0100

    lint fixes

--- 2019/day_02/2.py
+++ 2019/day_02/2.py
@@ -3,15 +3,18 @@
 
 from itertools import product
 
+
 def _run_op(values, instruction_pointer):
     values[values[instruction_pointer + 3]] = {
         1: values[values[instruction_pointer + 1]] + values[values[instruction_pointer + 2]],
-        2: values[values[instruction_pointer + 1]]*values[values[instruction_pointer + 2]]
+        2: values[values[instruction_pointer + 1]] * values[values[instruction_pointer + 2]]
     }.get(values[instruction_pointer])
+
 
 def _init(values, noun, verb):
     values[1] = noun
     values[2] = verb
+
 
 def _run_prog(values, noun, verb):
     _init(values, noun, verb)
@@ -21,17 +24,19 @@ def _run_prog(values, noun, verb):
         _run_op(values, i)
     return values[0]
 
+
 def part1(values):
     return _run_prog(values, 12, 2)
+
 
 def part2(values):
     for noun, verb in product(range(100), range(100)):
         if _run_prog(list(values), noun, verb) == 19690720:
-            return 100*noun + verb
+            return 100 * noun + verb
+
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         values = list(map(int, f.read().split(',')))
-    print(part1(list(values))) # 6087827
-    print(part2(values)) # 5379
-
+    print(part1(list(values)))  # 6087827
+    print(part2(values))  # 5379
