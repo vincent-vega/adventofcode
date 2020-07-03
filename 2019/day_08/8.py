798141d8102fc8c8579b1e5494feae88e66a1dbb
Davide Barelli <bar.davide@gmail.com>
2020-07-03 20:11:58 +0100

    lint fixes

--- 2019/day_08/8.py
+++ 2019/day_08/8.py
@@ -4,26 +4,29 @@
 from itertools import product
 from collections import Counter, defaultdict
 
+
 def part1(values: list, L: int, H: int) -> int:
-    layer_size = L*H
+    layer_size = L * H
     layer = min([ Counter(values[i:i + layer_size]) for i in range(1, len(values), layer_size) ], key=lambda c: c[0])
-    return layer[1]*layer[2]
+    return layer[1] * layer[2]
+
 
 def _draw(pixel: int) -> str:
     return '#' if pixel == 1 else ' '
 
+
 def part2(values: list, L: int, H: int) -> str:
-    I = defaultdict(lambda: 2)
-    layer_size = L*H
+    image = defaultdict(lambda: 2)
+    layer_size = L * H
     for i in range(0, len(values), layer_size):
-        for row, col in filter(lambda x: I[(x[1], x[0])] == 2, product(range(H), range(L))):
+        for row, col in filter(lambda x: image[(x[1], x[0])] == 2, product(range(H), range(L))):
             layer = values[i:i + layer_size]
-            I[(col, row)] = layer[row*L + col]
-    return '\n'.join([ ''.join([ _draw(I[k]) for k in I.keys() if k[1] == i ]) for i in range(H) ])
+            image[(col, row)] = layer[row * L + col]
+    return '\n'.join([ ''.join([ _draw(image[k]) for k in image.keys() if k[1] == i ]) for i in range(H) ])
+
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         values = [ int(n) for n in f.read().splitlines()[0] ]
-    print(part1(values, 25, 6)) # 2440
-    print(part2(values, 25, 6)) # AZCJC
-
+    print(part1(values, 25, 6))  # 2440
+    print(part2(values, 25, 6))  # AZCJC
