b1ef8a56aa7828c675ee6437dcee5f8cb8d82861
Davide Barelli <bar.davide@gmail.com>
2020-07-25 15:32:02 +0100

    clean

--- 2019/day_18/18.py
+++ 2019/day_18/18.py
@@ -5,13 +5,8 @@ from collections import defaultdict, namedtuple, deque
 import re
 
 
-def _get_directions(coord: (int, int)) -> tuple:
-    x, y = coord
-    return (x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)
-
-
 def part1(M: dict) -> int:
-    keys = [ k for k in M.keys() if re.match(r'[a-z]', M[k]) ]
+    keys = [ k for k in M.keys() if _is_key(M[k]) ]
     start = next(filter(lambda k: M[k] == '@', M.keys()))
     KeyFinder = namedtuple('KeyFinder', [ 'coord', 'keys', 'steps' ])
     Q = deque([ KeyFinder(start, set(), 0) ])
@@ -23,19 +18,30 @@ def part1(M: dict) -> int:
             continue
         history.add(state)
         curr_char = M[cur.coord]
-        if re.match(r'[a-z]', curr_char) and curr_char not in cur.keys:
-            # key
+        if _is_key(curr_char) and curr_char not in cur.keys:
             cur.keys.add(curr_char)
             if len(cur.keys) == len(keys):
                 return cur.steps
-        elif re.match(r'[A-Z]', curr_char):
-            # door
-            need_key = chr(ord(curr_char) + 32)
+        elif _is_door(curr_char):
+            need_key = curr_char.swapcase()
             if need_key not in cur.keys:
                 continue
-        for child in filter(lambda d: d in M.keys(), _get_directions(cur.coord)):
-            Q.append(KeyFinder(child, set(cur.keys), cur.steps + 1))
+        for coord in filter(lambda d: d in M.keys(), _get_adj_coord(cur.coord)):
+            Q.append(KeyFinder(coord, set(cur.keys), cur.steps + 1))
     return -1
+
+
+def _is_key(char: str) -> bool:
+    return re.match(r'[a-z]', char)
+
+
+def _is_door(char: str) -> bool:
+    return re.match(r'[A-Z]', char)
+
+
+def _get_adj_coord(coord: (int, int)) -> tuple:
+    x, y = coord
+    return (x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)
 
 
 def _parse(values: list) -> dict:
