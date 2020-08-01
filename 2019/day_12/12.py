c1d28710a8980dd942e8bf3a1cbe445b0f21f622
Davide Barelli <bar.davide@gmail.com>
2020-08-01 15:08:47 +0100

    clean

--- 2019/day_12/12.py
+++ 2019/day_12/12.py
@@ -4,13 +4,14 @@
 from functools import reduce
 from itertools import combinations, count
 from math import gcd
-import matplotlib.pyplot as plt
 import re
+
 
 def part1(moons: list, steps: int) -> int:
     for _ in range(steps):
         _run(moons)
-    return sum([ m.potential_e()*m.kinetic_e() for m in moons ])
+    return sum([ m.potential_e() * m.kinetic_e() for m in moons ])
+
 
 def part2(moons: list) -> int:
     # Moons' position/velocity are periodic
@@ -28,8 +29,10 @@ def part2(moons: list) -> int:
 
     return reduce(_lcm, periods)
 
+
 def _lcm(a, b):
-    return abs(a*b) // gcd(a, b)
+    return abs(a * b) // gcd(a, b)
+
 
 def _run(moons: list):
     for m1, m2 in combinations(moons, 2):
@@ -39,11 +42,13 @@ def _run(moons: list):
     for m in moons:
         m.move()
 
+
 def _gravities(m1: 'Moon', m2: 'Moon') -> list:
     def _pull(x: (int, int)) -> int:
         return 1 if x[1] > x[0] else -1 if x[1] < x[0] else 0
     g1 = [ _pull(x) for x in zip(m1.location(), m2.location()) ]
-    return g1, [ -1*x for x in g1 ]
+    return g1, [ -1 * x for x in g1 ]
+
 
 class Moon:
 
@@ -77,46 +82,9 @@ class Moon:
     def potential_e(self) -> int:
         return sum([ abs(i) for i in (self.x, self.y, self.z) ])
 
-def plot(moons: list):
-    x = [ moons[0].x ]
-    y = [ moons[0].y ]
-    z = [ moons[0].z ]
-    vx = [ moons[0].vx ]
-    vy = [ moons[0].vy ]
-    vz = [ moons[0].vz ]
-    for _ in range(1000):
-        _run(moons)
-        x.append(moons[0].x)
-        y.append(moons[0].y)
-        z.append(moons[0].z)
-        vx.append(moons[0].vx)
-        vy.append(moons[0].vy)
-        vz.append(moons[0].vz)
-
-    plt.figure()
-
-    plt.subplot(311)
-    plt.title('x')
-    plt.plot(x)
-
-    plt.plot(vx, 'r--')
-
-    plt.subplot(312)
-    plt.title('y')
-    plt.plot(y)
-
-    plt.plot(vy, 'r--')
-
-    plt.subplot(313)
-    plt.title('z')
-    plt.plot(z)
-
-    plt.plot(vz, 'r--')
-    plt.show()
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         moons = [ Moon(*p) for p in map(lambda l: [ int(x) for x in re.findall('-?\\d+', l) ], f.read().splitlines()) ]
-    print(part1(list(moons), 1000)) # 14907
-    print(part2(list(moons))) # 467081194429464
-
+    print(part1(list(moons), 1000))  # 14907
+    print(part2(list(moons)))  # 467081194429464
