4d0ad200c9d692bd784259b476aa77c5caab8964
Davide Barelli <bar.davide@gmail.com>
2020-12-17 21:32:16 +0000

    Performance improvements * detas caching * cubes frequencies

--- 2020/day_17/17.py
+++ 2020/day_17/17.py
@@ -1,48 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from collections import Counter
 from itertools import combinations
+
+
+def _deltas(size: int, cache: dict={}) -> set:
+    if 'deltas' in cache:
+        return cache['deltas']
+    zero_delta = (0,) * size
+    cache['deltas'] = { c for c in combinations([-1, 0, 1] * size, size) if c != zero_delta }
+    return cache['deltas']
 
 
 def _neighbors(coord: tuple, cache: dict={}) -> set:
     if coord in cache:
         return cache[coord]
-    zero_delta = (0,) * len(coord)
-    deltas = { c for c in combinations([-1, 0, 1] * len(coord), len(coord)) if c != zero_delta }
-    cache[coord] = { tuple( c - d for c, d in zip(coord, delta)) for delta in deltas }
+    cache[coord] = { tuple( c - d for c, d in zip(coord, delta)) for delta in _deltas(len(coord), cache) }
     return cache[coord]
 
 
-def _cycle(cubes: dict, cache: dict={}) -> dict:
-    nxt = {}
-    W = { cc for c in cubes.keys() for cc in _neighbors(c, cache) } | set(cubes.keys())
-    for coord in W:
-        active = cubes.get(coord)
-        neigh = sum([ 1 for n in _neighbors(coord, cache) if cubes.get(n) ])
-        if active and (neigh == 2 or neigh == 3):
-            nxt[coord] = True
-        elif not active and neigh == 3:
-            nxt[coord] = True
+def _cycle(cubes: dict, rounds: int=6) -> dict:
+    cache = {}
+    for _ in range(rounds):
+        nxt = {}
+        cubes_freq = Counter([ cc for c in cubes.keys() for cc in _neighbors(c, cache) ])
+        for coord, neigh in cubes_freq.items():
+            active = cubes.get(coord)
+            if active and (neigh == 2 or neigh == 3):
+                nxt[coord] = True
+            elif not active and neigh == 3:
+                nxt[coord] = True
+        cubes = nxt
     return nxt
-
-
-def part1(cubes: dict, rounds: int=6) -> int:
-    cache = {}
-    for _ in range(rounds):
-        cubes = _cycle(cubes, cache)
-    return len(cubes.keys())
-
-
-def part2(cubes: dict, rounds: int=6) -> int:
-    cache = {}
-    for _ in range(rounds):
-        cubes = _cycle(cubes, cache)
-    return len(cubes.keys())
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         lines = f.read().splitlines()
         cubes = { (0, y, x): True for y in range(len(lines)) for x, s in enumerate(lines[y]) if s == '#' }
-    print(part1(cubes))  # 295
-    print(part2({ (0, *c): True for c in cubes }))  # 1972
+    print(len(_cycle(cubes)))  # 295
+    print(len(_cycle({ (0, *c): True for c in cubes })))  # 1972
