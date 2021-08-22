69f9383ef2a0f4628c0b905aa6aa98b2f40a0b45
Davide Barelli <bar.davide@gmail.com>
2021-08-22 15:33:39 +0100

    day 12

---
+++ 2017/day_12/12.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def _parse(line: str) -> set:
+    _, ids = line.split(' <-> ')
+    return set([ int(i) for i in ids.split(', ') ])
+
+
+def _scan(programs: dict, seen: set, target: int=0) -> set:
+    seen.add(target)
+    group = set([ target ])
+    to_scan = programs[target] - seen
+    group.update(to_scan)
+    group.update({ nn for n in to_scan for nn in _scan(programs, seen, n) })
+    return group
+
+
+def part1(programs: dict):
+    return len(_scan(programs, {0}, 0))
+
+
+def part2(programs: dict):
+    groups = []
+    left = set(programs.keys())
+    while left:
+        p = left.pop()
+        g = _scan(programs, {p}, p)
+        left -= g
+        groups.append(g)
+    return len(groups)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        programs = { n: _parse(x) for n, x in enumerate(f.read().splitlines()) }
+    print(part1(programs))  # 283
+    print(part2(programs))  # 195
