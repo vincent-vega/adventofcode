a9eb31e2a2959fc978fc6cd4ff97f8b8c720a760
Davide Barelli <bar.davide@gmail.com>
2021-10-06 16:15:54 +0100

    parsing improved

--- 2018/day_03/3.py
+++ 2018/day_03/3.py
@@ -1,44 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
 import re
 
+Claim = namedtuple('Claim', [ 'id', 'offset_x', 'offset_y', 'width', 'height' ])
 
-def part1(values: list) -> int:
-    area = [ [ 0 for x in range(1000) ] for y in range(1000) ]
+
+def part1(claims: tuple) -> int:
+    area = tuple([ 0 for x in range(1000) ] for y in range(1000))
     count = 0
-    for claim in values:
-        y, x = map(int, re.search(r'^.+\s(\d+,\d+):.+$', claim).group(1).split(','))
-        dim_y, dim_x = map(int, re.search(r'^.+:\s(\d+x\d+)$', claim).group(1).split('x'))
-        for r in range(x, x + dim_x):
-            for c in range(y, y + dim_y):
-                if area[r][c] == 1:
-                    area[r][c] += 1
+    for c in claims:
+        for x in range(c.offset_x, c.offset_x + c.width):
+            for y in range(c.offset_y, c.offset_y + c.height):
+                if area[x][y] == 1:
+                    area[x][y] += 1
                     count += 1
-                elif area[r][c] == 0:
-                    area[r][c] += 1
+                elif area[x][y] == 0:
+                    area[x][y] += 1
     return count
 
 
-def part2(values: list) -> int:
-    def extract(s):
-        return tuple(int(x) for x in re.findall(r'-?\d+', s))
+def part2(claims: tuple) -> int:
     area = {}
     double = set()
-    data = [ extract(claim) for claim in values ]
-    for n, x, y, l, w in data:
-        for i in range(l):
-            for j in range(w):
-                offset = i + x, j + y
+    for c in claims:
+        for i in range(c.width):
+            for j in range(c.height):
+                offset = i + c.offset_x, j + c.offset_y
                 if area.get(offset, 0) == 0:
-                    area[offset] = n
+                    area[offset] = c.id
                 else:
-                    double.update([ n, area.get(offset, 0) ])
-    return min(set(range(1, len(data) + 1)) - double)
+                    double.update([ c.id, area.get(offset, 0) ])
+    return min(set(range(1, len(claims) + 1)) - double)
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        values = f.read().splitlines()
-    print(part1(values))  # 119551
-    print(part2(values))  # 1124
+        claims = tuple(Claim(*list(map(int, re.findall(r'\d+', line)))) for line in f.read().splitlines())
+    print(part1(claims))  # 119551
+    print(part2(claims))  # 1124
