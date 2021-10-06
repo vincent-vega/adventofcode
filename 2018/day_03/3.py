701f450d675d31690891aefb790574ed99439597
Davide Barelli <bar.davide@gmail.com>
2021-10-06 15:51:40 +0100

    fixes

--- 2018/day_03/3.py
+++ 2018/day_03/3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from collections import defaultdict
 import re
 
 
@@ -9,10 +8,8 @@ def part1(values: list) -> int:
     area = [ [ 0 for x in range(1000) ] for y in range(1000) ]
     count = 0
     for claim in values:
-        coord = re.search('^.+\s([0-9]+,[0-9]+):.+$', claim).group(1)
-        y, x = [ int(n) for n in coord.split(',') ]
-        size = re.search('^.+:\s([0-9]+x[0-9]+)$', claim).group(1)
-        dim_y, dim_x = [ int(n) for n in size.split('x') ]
+        y, x = map(int, re.search('^.+\s([0-9]+,[0-9]+):.+$', claim).group(1).split(','))
+        dim_y, dim_x = map(int, re.search('^.+:\s([0-9]+x[0-9]+)$', claim).group(1).split('x'))
         for r in range(x, x + dim_x):
             for c in range(y, y + dim_y):
                 if area[r][c] == 1:
@@ -25,19 +22,19 @@ def part1(values: list) -> int:
 
 def part2(values: list) -> int:
     def extract(s):
-        return [ int(x) for x in re.findall(r'-?\d+', s) ]
-    area = defaultdict(int)
-    double = defaultdict(bool)
+        return tuple(int(x) for x in re.findall(r'-?\d+', s))
+    area = {}
+    double = set()
     data = [ extract(claim) for claim in values ]
     for n, x, y, l, w in data:
         for i in range(l):
             for j in range(w):
                 offset = i + x, j + y
-                if area[offset] == 0:
+                if area.get(offset, 0) == 0:
                     area[offset] = n
                 else:
-                    double[n] = double[area[offset]] = True
-    return [ x for x in range(1, len(data) + 1) if x not in double.keys() ][0]
+                    double.update([ n, area.get(offset, 0) ])
+    return min(set(range(1, len(data) + 1)) - double)
 
 
 if __name__ == '__main__':
