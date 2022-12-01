f0b090ef0ef84b3d246e4c935f831850be18c6df
Davide Barelli <bar.davide@gmail.com>
2022-12-01 07:45:22 +0100

    2022 day 1

---
+++ 2022/day_01/1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def part1(values: list[int]) -> int:
+    return max(sum(elf) for elf in values)
+
+
+def part2(values: list[int]) -> int:
+    return sum(sorted(sum(elf) for elf in values)[-3:])
+
+
+def _parse(elf: str) -> list:
+    return list(map(int, elf.split()))
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        values = [ _parse(e.strip()) for e in f.read().split('\n\n') ]
+    print(part1(values))  # 67450
+    print(part2(values))  # 199357
