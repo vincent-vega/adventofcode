fccd33adb80a23eb847c1c8d819ea9207c3acd01
Davide Barelli <bar.davide@gmail.com>
2022-12-03 19:07:08 +0100

    clean

--- 2022/day_03/3.py
+++ 2022/day_03/3.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*-
 
 def _priority(item: str) -> int:
-    return ord(item) - 96 if item.islower() else ord(item) - 38
+    return ord(item) - ord('a') + 1 if item.islower() else ord(item) - ord('A') + 27
 
 
 def part1(rucksacks: list[str]) -> int:
-    rucksacks = [ (set(r[:len(r) // 2]), set(r[len(r) // 2:])) for r in rucksacks ]
-    return sum(_priority(b1.intersection(b2).pop()) for b1, b2 in rucksacks)
+    rucksacks = map(lambda r: (set(r[:len(r) // 2]), set(r[len(r) // 2:])), rucksacks)
+    return sum(_priority(comp1.intersection(comp2).pop()) for comp1, comp2 in rucksacks)
 
 
-def _badge(group: list[str]) -> int:
-    return group[0].intersection(group[1]).intersection(group[2])
+def _badge(group: list[set[str]]) -> str:
+    return group[0].intersection(group[1]).intersection(group[2]).pop()
 
 
 def part2(rucksacks: list[str]) -> int:
-    return sum(_priority(_badge([ set(r) for r in rucksacks[i:i + 3] ]).pop()) for i in range(0, len(rucksacks), 3))
+    return sum(_priority(_badge(list(map(set, rucksacks[i:i + 3])))) for i in range(0, len(rucksacks), 3))
 
 
 if __name__ == '__main__':
