74aced791248c71773b68f6e8b787ce69adf991a
Davide Barelli <bar.davide@gmail.com>
2023-12-09 13:16:18 +0100

    clean

--- 2023/day_09/9.py
+++ 2023/day_09/9.py
@@ -8,20 +8,20 @@ def _nxt(values: tuple[int]) -> tuple[int]:
     return tuple(values[i + 1] - values[i] for i in range(len(values) - 1))
 
 
-def _seq(values: tuple[int], first: bool = False) -> tuple[int]:
-    sequence = [ tuple(values) ]
+def _seq(values: tuple[int], first: bool = False) -> list[int]:
+    sequence = [ values[0] if first else values[-1] ]
     while True:
         nxt = _nxt(values)
         for n in (x for x in nxt if x != 0):
             break
         else:
             break
-        sequence.append(nxt)
+        sequence.append(nxt[0] if first else nxt[-1])
         values = nxt
-    return tuple(s[0] if first else s[-1] for s in sequence)
+    return sequence
 
 
-def _predict1(sequence: tuple[int], diff: int = 0) -> int:
+def _predict1(sequence: list[int], diff: int = 0) -> int:
     for i in range(len(sequence) - 1, -1, -1):
         diff += sequence[i]
     return diff
@@ -31,7 +31,7 @@ def part1(history: tuple[tuple[int]]) -> int:
     return sum(_predict1(_seq(h)) for h in history)
 
 
-def _predict2(sequence: tuple[int], diff: int = 0) -> int:
+def _predict2(sequence: list[int], diff: int = 0) -> int:
     for i in range(len(sequence) - 1, -1, -1):
         diff = sequence[i] - diff
     return diff
