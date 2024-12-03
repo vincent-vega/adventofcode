60ade8283c97ba73787e20fa576241efe762be37
Davide Barelli <bar.davide@gmail.com>
2024-12-03 19:16:44 +0100

    clean

--- 2024/day_03/3.py
+++ 2024/day_03/3.py
@@ -5,22 +5,22 @@ import re
 
 
 def part1(memory: str) -> int:
-    instructions = re.findall(r'mul\(\d{1,3},\d{1,3}\)', memory)
+    instructions = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', memory)
     result = 0
     for i in instructions:
-        x, y = map(int, re.findall(r'\d+', i))
+        x, y = map(int, i)
         result += x * y
     return result
 
 
 def part2(memory: str) -> int:
-    instructions = re.findall(r'(mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\))', memory)
+    instructions = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", memory)
     result = 0
     enabled = True
     for i in instructions:
         if i == 'do()':
             enabled = True
-        elif i == 'don\'t()':
+        elif i == "don't()":
             enabled = False
         elif enabled:
             x, y = map(int, re.findall(r'\d+', i))
