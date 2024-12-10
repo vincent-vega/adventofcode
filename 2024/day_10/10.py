4a40b9551ddf1fb7ec193fcd652872281c4b526f
Davide Barelli <bar.davide@gmail.com>
2024-12-10 08:53:57 +0100

    2024 day 10

---
+++ 2024/day_10/10.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from common.lib import _adj
+
+
+def _score(heights: dict[tuple[int, int], int], start: tuple[int, int]) -> int:
+    score = 0
+    visited = set()
+    D = [ (start, 0) ]
+    while D:
+        cur, n = D.pop()
+        for nxt in (nxt for nxt in _adj(*cur) if nxt in heights and heights[nxt] == n + 1):
+            if heights[nxt] == 9 and nxt not in visited:
+                score += 1
+                visited.add(nxt)
+            else:
+                D.append((nxt, n + 1))
+    return score
+
+
+def _rating(heights: dict[tuple[int, int], int], start: tuple[int, int]) -> int:
+    score = 0
+    seen = set()
+    D = [ (start, 0, [ start ]) ]
+    while D:
+        cur, n, path = D.pop()
+        for nxt in (nxt for nxt in _adj(*cur) if nxt in heights and heights[nxt] == n + 1):
+            path.append(nxt)
+            if heights[nxt] == 9 and tuple(path) not in seen:
+                score += 1
+                seen.add(tuple(path))
+            else:
+                D.append((nxt, n + 1, path))
+    return score
+
+
+def part1(heights: dict[tuple[int, int], int]) -> int:
+    return sum(_score(heights, c) for c, h in heights.items() if h == 0)
+
+
+def part2(heights: dict[tuple[int, int], int]) -> int:
+    return sum(_rating(heights, c) for c, h in heights.items() if h == 0)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        heights = { (x, y): h for y, line in enumerate(f.read().splitlines()) for x, h in enumerate(map(int, line)) }
+    print(part1(heights))  # 582
+    print(part2(heights))  # 1302
--- common/lib.py
+++ common/lib.py
@@ -1,10 +1,17 @@
 import pyperclip
+from functools import cache
+
+
+@cache
+def _adj(x: int, y: int) -> list[tuple[int, int]]:
+    return [ (x + dx, y + dy) for dx in (-1, 0, 1) for dy in (-1, 0, 1) if abs(dx) != abs(dy) ]
+
+
+@cache
+def _manhattan(x1: int, y1: int, x2: int, y2: int) -> int:
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
 def _print(s: str):
     pyperclip.copy(s)
     print(s)
-
-
-def _manhattan(x1: int, y1: int, x2: int, y2: int) -> int:
-    return abs(x1 - x2) + abs(y1 - y2)
