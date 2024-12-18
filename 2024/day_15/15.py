237af2bf2304e8125d2039fcb82f114cfa5f567a
Davide Barelli <bar.davide@gmail.com>
2024-12-18 21:11:43 +0100

    clean

--- 2024/day_15/15.py
+++ 2024/day_15/15.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from common.lib import _adj
 from dataclasses import dataclass
-from functools import cache
-
-
-@dataclass
-class Box:
-    coord: tuple[tuple[int]]
 
 
 def _nxt(x, y, d):
@@ -22,7 +15,7 @@ def _nxt(x, y, d):
         return x - 1, y
 
 
-def part1(robot, walls, boxes, moves):
+def part1(robot: tuple[int], walls: set[tuple[int]], boxes: set[tuple[int]], moves: str) -> int:
     for m in moves:
         c = _nxt(*robot, m)
         boxes_to_move = set()
@@ -37,29 +30,12 @@ def part1(robot, walls, boxes, moves):
     return sum(x + 100 * y for x, y in boxes)
 
 
-@cache
-def _around(coord: tuple[tuple[int]]) -> set[tuple[int]]:
-    return set(a for c in coord for a in _adj(*c)) - set(coord)
+@dataclass
+class Box:
+    coord: tuple[tuple[int]]
 
 
-def _show(walls, boxes, robot):
-    X, _ = max(walls, key=lambda c: c[0])
-    _, Y = max(walls, key=lambda c: c[1])
-    boxes_coord = { c for b in boxes for c in b.coord }
-
-    def _get_c(x, y):
-        for b in boxes:
-            b1, b2 = b.coord
-            if (x, y) == b1:
-                return '['
-            elif (x, y) == b2:
-                return ']'
-
-    for y in range(Y + 1):
-        print(*[ '#' if (x, y) in walls else _get_c(x, y) if (x, y) in boxes_coord else '@' if (x, y) == robot else '.' for x in range(X + 1) ], sep='')
-
-
-def part2(robot, walls, boxes, moves):
+def part2(robot: tuple[int], walls: set[tuple[int]], boxes: set[tuple[int]], moves: str) -> int:
     robot = (2 * robot[0], robot[1])
     walls = { (2 * x + dx, y) for x, y in walls for dx in (0, 1) }
     boxes = [ Box(((2 * x, y), (2 * x + 1, y))) for x, y in boxes ]
@@ -86,9 +62,9 @@ def part2(robot, walls, boxes, moves):
         elif move in '<>' and nxt not in walls:
             robot = nxt
         elif move in '^v' and nxt in boxes_coord:
+            dy = 1 if move == 'v' else -1
             boxes_to_move = []
             coord_to_check = set([ nxt ])
-            dy = 1 if move == 'v' else -1
             changed = True
             cannot_move = False
             while changed:
@@ -113,7 +89,7 @@ def part2(robot, walls, boxes, moves):
             robot = nxt
         elif move in '^v' and nxt not in walls:
             robot = nxt
-    return sum(b1[0] + 100 * b1[1] for b1, b2 in map(lambda b: b.coord, boxes))
+    return sum(x + 100 * y for x, y in map(lambda b: b.coord[0], boxes))
 
 
 if __name__ == '__main__':
@@ -130,7 +106,5 @@ if __name__ == '__main__':
                 elif c == 'O':
                     boxes.add((x, y))
         moves = ''.join(map(str.strip, moves))
-    assert part1(robot, walls, boxes, moves) == 1526018
-    assert part2(robot, walls, boxes, moves) == 1550677
-    # _print(part1(robot, walls, boxes, moves))  # 1526018
-    # _print(part2(robot, walls, boxes, moves))  # 1550677
+    print(part1(robot, walls, boxes, moves))  # 1526018
+    print(part2(robot, walls, boxes, moves))  # 1550677
