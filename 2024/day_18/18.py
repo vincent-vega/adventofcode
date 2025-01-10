c3338e920d22fb8b8787cc3b7c7b026c0927067e
Davide Barelli <bar.davide@gmail.com>
2025-01-10 18:22:12 +0100

    fix

--- 2024/day_18/18.py
+++ 2024/day_18/18.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from common.lib import manhattan, adj
+from common.lib import adj, manhattan
 import heapq
 
 
-def _escape(walls: set[tuple[int, int]], X: int, Y: int) -> int:
-    Q = [(0, 0, 0, 0)]
+def _escape(walls: set[tuple[int, int]], X: int, Y: int) -> tuple[int, set[tuple[int, int]]]:
+    Q = [(0, 0, 0, 0, {(0, 0)})]
     visited = set()
     while Q:
-        _, steps, x, y = heapq.heappop(Q)
+        _, steps, x, y, path = heapq.heappop(Q)
         if (x, y) in visited:
             continue
         if (x, y) == (X, Y):
-            return steps
+            return steps, path
         visited.add((x, y))
         steps += 1
         for xx, yy in ((xx, yy) for xx, yy in adj(x, y) if (xx, yy) not in visited and 0 <= xx <= X and 0 <= yy <= Y and (xx, yy) not in walls):
-            heapq.heappush(Q, (steps + manhattan(xx, yy, X, Y), steps, xx, yy))
-    return -1
+            heapq.heappush(Q, (steps + manhattan(xx, yy, X, Y), steps, xx, yy, path | {(xx, yy)}))
+    return -1, None
 
 
-def part1(memory: list[tuple[int, int]], X: int, Y: int, N: int) -> int:
-    return _escape({ memory[n] for n in range(N) }, X, Y)
+def part1(memory: list[tuple[int, int]], X: int, Y: int, count: int) -> int:
+    s, _ = _escape({ memory[n] for n in range(count) }, X, Y)
+    return s
 
 
 def part2(memory: list[tuple[int, int]], X: int, Y: int, count: int) -> str:
     walls = { memory[n] for n in range(count) }
     while count < len(memory):
-        if _escape(walls, X, Y) < 0:
+        s, path = _escape(walls, X, Y)
+        if s < 0:
             x, y = memory[count - 1]
             return f'{x},{y}'
-        walls.add(memory[count])
-        count += 1
+        while True:
+            walls.add(memory[count])
+            count += 1
+            if count == len(memory) or memory[count - 1] in path:
+                break
     raise Exception('Always reachable')
 
 
